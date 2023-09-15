fe8c29b439dd4ed41c0c7438076d5d4e1b7ab4a0
Sebastian Tombe Campo <116851539+SebasTombe10@users.noreply.github.com>
2023-09-15 16:35:25 -0500

    voraz-funcion

--- Ventana_principal.py
+++ Ventana_principal.py
@@ -1,4 +1,5 @@
 from Cargar_archivo import cargar
+from voraz import voraz
 from tkinter import *
 from tkinter import ttk
 root = Tk()
@@ -6,6 +7,6 @@ frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Button(frm, text="Cargar datos", command=cargar ).grid(column=1, row=0)
 ttk.Button(frm, text="Fuerza Bruta", command=root.destroy).grid(column=1, row=1)
-ttk.Button(frm, text="Voraz", command=root.destroy).grid(column=1, row=2)
+ttk.Button(frm, text="Voraz", command=voraz).grid(column=1, row=2)
 ttk.Button(frm, text="Dinamica", command=root.destroy).grid(column=1, row=3)
 root.mainloop() 
