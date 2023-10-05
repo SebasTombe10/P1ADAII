e89b8af33e6c0d71f201bef2066a7f7e73a7c620
Sebastian Tombe Campo <116851539+SebasTombe10@users.noreply.github.com>
2023-10-04 19:03:08 -0500

    ajuste en rocV

--- voraz.py
+++ voraz.py
@@ -7,9 +7,10 @@ def voraz(d):
     ta = copy.deepcopy(dicestudiantes)
     asignadas = {estudiante: [] for estudiante in dicestudiantes.keys()}
 
+    while True:
         #Los chances que tiene cada estudiante para obtener una asifnatura
-    chances = {}
-    for estudiantes_chances in dicestudiantes:
+        chances = {}
+        for estudiantes_chances in dicestudiantes:
             suma = 0
             asignaturas_solicitadas_chances = dicestudiantes[estudiantes_chances]
             for codigo,_ in asignaturas_solicitadas_chances:
@@ -18,10 +19,11 @@ def voraz(d):
                     suma = suma + cupo
             chances.update({estudiantes_chances:suma})
         # Ordenar el diccionario por valores de menor a mayor
-    chances_ordenado = dict(sorted(chances.items(), key=lambda item: item[1]))
-    for estudiante in chances_ordenado:
+        chances_ordenado = dict(sorted(chances.items(), key=lambda item: item[1]))
+        for estudiante in chances_ordenado:
             asignaturas_solicitadas = dicestudiantes[estudiante]
-            for codigo,prioridad in asignaturas_solicitadas:
+            asignaturas_solicitadas_ordenadas = sorted(asignaturas_solicitadas, key=lambda x: x[1], reverse=True)
+            for codigo,prioridad in asignaturas_solicitadas_ordenadas:
                     if asignaturas[codigo] > 0:
                         if codigo not in asignadas[estudiante]:
                             asignadas[estudiante].append(codigo)
@@ -29,6 +31,7 @@ def voraz(d):
                             lista_asignatura_solicitada.remove((codigo,prioridad))
                             asignaturas[codigo] = asignaturas[codigo]-1
             
+        if all(valor == 0 for valor in asignaturas.values()):break
     suma_prioridad_no_asignadas = []
 
     for lista_de_tuplas in dicestudiantes.values():
