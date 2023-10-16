9853b1c8cf1dfe1bc11e6abc17de203f6e732835
Velyxius <dadawdasd@hotmail.com>
2023-10-16 18:47:18 -0500

    Modified Greedy Algorithm

--- rocV.py
+++ rocV.py
@@ -1,45 +1,74 @@
-from insatisfaccion import insaEstudiante
-import copy
+from copy import deepcopy
 
-def voraz(d):
+
+def insaEstudiante(
+    asignaciones: dict[str, list],
+    solicitudes: dict[str, list[tuple[str, int]]],
+    suma_prioridad_no_asignadas: list[int],
+):
+    listres = []
+    insatisfaccion = 0
+    listma = [len(lista) for lista in asignaciones.values()]
+    listms = [len(lista1) for lista1 in solicitudes.values()]
+    c = 0
+    for i in range(len(suma_prioridad_no_asignadas)):
+        res = (1 - (abs(listma[i]) / abs(listms[i]))) * (
+            suma_prioridad_no_asignadas[i] / ((3 * abs(listms[i])) - 1)
+        )
+        listres.append(res)
+        if len(listres) == len(listms):
+            break
+
+    suma_general = sum(listres)
+    insatisfaccion = suma_general / len(suma_prioridad_no_asignadas)
+    return insatisfaccion
+
+
+def rocV(
+    datos: tuple[dict[str, int], dict[str, list[tuple[str, int]]]]
+) -> tuple[dict[str, list[str]], float]:
     # clave cod asig : valor cupo asign
-    asignaturas,dicestudiantes = d
-    ta = copy.deepcopy(dicestudiantes)
-    asignadas = {estudiante: [] for estudiante in dicestudiantes.keys()}
+    materias = deepcopy(datos[0])
+    estudiantes = deepcopy(datos[1])
+    solicitudes = deepcopy(estudiantes)
+    asignaciones = {estudiante: [] for estudiante in estudiantes.keys()}
 
     while True:
-        #Los chances que tiene cada estudiante para obtener una asifnatura
+        # Los chances que tiene cada estudiante para obtener una materia
         chances = {}
-        for estudiantes_chances in dicestudiantes:
-            print("x")
+        for estudiantes_chances in estudiantes:
             suma = 0
-            asignaturas_solicitadas_chances = dicestudiantes[estudiantes_chances]
-            for codigo,_ in asignaturas_solicitadas_chances:
-                if (codigo in asignaturas):
-                    cupo = int(asignaturas[codigo])
+            materias_solicitadas_chances = estudiantes[estudiantes_chances]
+            for codigo, _ in materias_solicitadas_chances:
+                if codigo in materias:
+                    cupo = int(materias[codigo])
                     suma = suma + cupo
-            chances.update({estudiantes_chances:suma})
+            chances.update({estudiantes_chances: suma})
         # Ordenar el diccionario por valores de menor a mayor
         chances_ordenado = dict(sorted(chances.items(), key=lambda item: item[1]))
         for estudiante in chances_ordenado:
-            asignaturas_solicitadas = dicestudiantes[estudiante]
-            asignaturas_solicitadas_ordenadas = sorted(asignaturas_solicitadas, key=lambda x: x[1], reverse=True)
-            for codigo,prioridad in asignaturas_solicitadas_ordenadas:
-                    if asignaturas[codigo] > 0:
-                        if codigo not in asignadas[estudiante]:
-                            asignadas[estudiante].append(codigo)
-                            lista_asignatura_solicitada = dicestudiantes[estudiante]
-                            lista_asignatura_solicitada.remove((codigo,prioridad))
-                            asignaturas[codigo] = asignaturas[codigo]-1
-                            break
-            
-        if all(valor == 0 for valor in asignaturas.values()):break
+            materias_solicitadas = estudiantes[estudiante]
+            materias_solicitadas_ordenadas = sorted(
+                materias_solicitadas, key=lambda x: x[1], reverse=True
+            )
+            for codigo, prioridad in materias_solicitadas_ordenadas:
+                if materias[codigo] > 0:
+                    if codigo not in asignaciones[estudiante]:
+                        asignaciones[estudiante].append((codigo, prioridad))
+                        lista_asignatura_solicitada = estudiantes[estudiante]
+                        lista_asignatura_solicitada.remove((codigo, prioridad))
+                        materias[codigo] = materias[codigo] - 1
+                        break
+
+        if all(valor == 0 for valor in materias.values()):
+            break
     suma_prioridad_no_asignadas = []
 
-    for lista_de_tuplas in dicestudiantes.values():
+    for lista_de_tuplas in estudiantes.values():
         suma = sum(valor for _, valor in lista_de_tuplas)
         suma_prioridad_no_asignadas.append(suma)
 
-    insatisfaccion = insaEstudiante(asignadas,ta,suma_prioridad_no_asignadas)
-    asignadas.update({"Insatisfaccion":[str(insatisfaccion)]})
-    return asignadas
+    insatisfaccion = insaEstudiante(
+        asignaciones, solicitudes, suma_prioridad_no_asignadas
+    )
+    return asignaciones, insatisfaccion
